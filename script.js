08a3b01d98ce8a09e789a8d8f0296e5c89e02f67
João Giovani <giovannirs957@gmail.com>
2024-01-07 17:07:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// seuArquivo.js
-import mobileNavbar from "/scripts/mobileNavbar.js";
-import scrollSmooth from "/scripts/scrollSmooth.js";
-import animatePhone from "/scripts/animatePhone.js";
-// import showContainers from "/scripts/showContainers.js";
+
+import mobileNavbar from "./scripts/mobileNavbar.js";
+import scrollSmooth from "./scripts/scrollSmooth.js";
+import animatePhone from "./scripts/animatePhone.js";
+import showContainers from "./scripts/showContainers.js";
 
 window.addEventListener("load", function () {
   mobileNavbar();
   scrollSmooth();
   animatePhone();
-  // showContainers();
+  showContainers();
 });
