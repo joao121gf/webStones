68bdf65b4be4d457e3c6b2d279d99c18ada417ea
João Giovani <giovannirs957@gmail.com>
2024-01-07 15:23:49 -0300

    Update showContainers.js

--- scripts/showContainers.js
+++ scripts/showContainers.js
@@ -1,7 +1,7 @@
 export default function showContainers() {
   const gridLose = document.querySelector(".grid-lose");
   const services = document.querySelector(".father-services");
-  const projects = document.querySelector('.projects')
+  const projects = document.querySelector(".projects");
   const redLine = document.querySelectorAll(".red-line");
   const containers = [gridLose, services, projects];
   containers.forEach((i) => {
@@ -10,13 +10,14 @@ export default function showContainers() {
   });
 
   function chama() {
-    const height = gridLose.offsetTop;
-    const windowAct = window.scrollY;
-    console.log(window.scrollY);
+    const windowAct = window.pageYOffset || document.documentElement.scrollTop;
+    console.log(windowAct);
+
     if (windowAct > 450) {
       gridLose.classList.add("animaWindow");
       gridLose.style.opacity = 1;
     }
+
     if (windowAct > 1705) {
       services.style.opacity = 1;
       services.classList.add("animaWindow");
@@ -26,10 +27,10 @@ export default function showContainers() {
         }, 1000 * index);
       });
     }
+
     if (windowAct > 3600) {
       projects.classList.add("animaWindow");
       projects.style.opacity = 1;
-
     }
   }
   window.addEventListener("scroll", chama);
