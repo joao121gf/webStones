8302d45b9aea24fb1a9b97fb3d7f254bef97085e
João Giovani <giovannirs957@gmail.com>
2024-01-07 15:11:07 -0300

    projects animation

--- scripts/showContainers.js
+++ scripts/showContainers.js
@@ -1,8 +1,9 @@
 export default function showContainers() {
   const gridLose = document.querySelector(".grid-lose");
   const services = document.querySelector(".father-services");
+  const projects = document.querySelector('.projects')
   const redLine = document.querySelectorAll(".red-line");
-  const containers = [gridLose, services];
+  const containers = [gridLose, services, projects];
   containers.forEach((i) => {
     i.style.opacity = 0;
     i.style.transition = "opacity .3s ease";
@@ -25,6 +26,12 @@ export default function showContainers() {
         }, 1000 * index);
       });
     }
+    if (windowAct > 3600) {
+      projects.classList.add("animaWindow");
+      projects.style.opacity = 1;
+
+    }
+
   }
   window.addEventListener("scroll", chama);
 }
