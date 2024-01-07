4eada5a88b6c54486533dd5fcc1dff1571df37f1
João Giovani <giovannirs957@gmail.com>
2024-01-07 16:52:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 import mobileNavbar from "/scripts/mobileNavbar.js";
 import scrollSmooth from "/scripts/scrollSmooth.js";
 import animatePhone from "/scripts/animatePhone.js";
-import showContainers from "./scripts/showContainers.js";
+import showContainers from "/scripts/showContainers.js";
 
 window.addEventListener("load", function () {
   mobileNavbar();
