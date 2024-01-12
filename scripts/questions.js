eb2d016454dadb21b5d1b77fe914e5871904268f
João Giovani <giovannirs957@gmail.com>
2024-01-11 21:15:11 -0300

    animation Questions

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,15 +1,23 @@
 export default function questions() {
   const circleRed = document.querySelectorAll(".circle-red");
   const aws = document.querySelectorAll(".aws");
+  let bxQs = document.querySelectorAll(".bx-qs");
+  bxQs.forEach((i) => {
+    i.style.height = "61px";
+  });
 
   function showAws() {
-    console.log(circleRed[0]);
-    console.log(this)
-    for (var i = 1; i < circleRed.length + 1; i++) {
-      if (this == circleRed[i]){
-        aws[1].style.display = 'block'
-        console.log('ok')
-      }
+    let divResp = this.parentNode.parentNode.firstElementChild.children;
+    let fahter = this.parentNode.parentNode.firstElementChild
+    console.log(fahter)
+    let resp = divResp[1];
+
+    resp.classList.toggle("active");
+    if (resp.classList.contains("active")) {
+       fahter.style.height = "initial";
+    } else {
+       fahter.style.height = "61px";
+       circleRed.style.border = '2px solid green'
     }
   }
   circleRed.forEach((i) => {
