d75569dab7f5b62318390d4c0074fc788d144c00
João Giovani <giovannirs957@gmail.com>
2024-01-12 20:46:18 -0300

    update responsive

--- scripts/questions.js
+++ scripts/questions.js
@@ -12,8 +12,7 @@ export default function questions() {
     let divResp = this.parentNode.parentNode.firstElementChild.children;
     let fahter = this.parentNode.parentNode.firstElementChild;
     let f = this.parentNode.parentNode;
-    let seta = this
-    console.log(this)
+    let seta = this;
     let resp = divResp[1];
 
     const isOpen = resp.classList.contains("active");
@@ -30,7 +29,6 @@ export default function questions() {
     } else {
       f.style.paddingBottom = "8px";
       this.style.border = "2px solid #939191";
-
     }
   }
 
@@ -39,10 +37,12 @@ export default function questions() {
       const divResp = i.parentNode.parentNode.firstElementChild.children;
       const resp = divResp[1];
       const fahter = i.parentNode.parentNode.firstElementChild;
+      console.log(i);
+      i.style.border = "2px solid #939191";
+
       boxQsr.forEach((i) => {
         i.style.paddingBottom = "8px";
       });
-
 
       resp.classList.remove("active");
       fahter.style.height = "61px";
